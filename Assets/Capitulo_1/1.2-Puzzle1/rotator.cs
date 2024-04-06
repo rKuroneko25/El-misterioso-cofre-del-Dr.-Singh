f65bcbd7c69447937423c485e3cba42469f75d89
Lynel <manuoteber@gmail.com>
2024-04-06 20:04:15 +0200

    odio photoshop, la postal ya rota, hay fondo y se pueden clickar los botones

---
+++ Assets/Capitulo_1/1.2-Puzzle1/rotator.cs
@@ -0,0 +1,57 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NewBehaviourScript : MonoBehaviour
+{   
+    [SerializeField] float rotationSpeed = 50f;
+    bool dragging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (!IsPointerOverText())
+        {
+            dragging = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(dragging)
+        {
+            float x = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            float y = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+            transform.Rotate(Vector3.down, x);
+            transform.Rotate(Vector3.right, y);
+        }
+    }
+
+    private bool IsPointerOverText()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        
+        TextMeshProUGUI[] textMeshPros = GetComponentsInChildren<TextMeshProUGUI>();
+        foreach (TextMeshProUGUI textMeshPro in textMeshPros)
+        {
+            if (RectTransformUtility.RectangleContainsScreenPoint(textMeshPro.rectTransform, Input.mousePosition, eventDataCurrentPosition.enterEventCamera))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
