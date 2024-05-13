3ceba498dcbc3d56114596a8eb93b9798dce2580
Lynel <manuoteber@gmail.com>
2024-05-13 21:46:31 +0200

    puzzle 5 le he puesto todas las animaciones y falta solo el audio

--- Assets/_Capitulo_2/2.2-Puzzle5/Giratuberia.cs
+++ Assets/_Capitulo_2/2.2-Puzzle5/Giratuberia.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Giratubería : MonoBehaviour
 {       
@@ -19,30 +20,51 @@ public class Giratubería : MonoBehaviour
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {
-            // Obtenemos el rayo desde la cámara al punto donde hicimos clic
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            
-            // Si el rayo golpea al GameObject
-            if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
-            {   Debug.Log("Tuberia clickeada");
-                // Activamos el trigger de la animación
-                position=(position+1) % 4;
-                // lanza la funcion triggerAnimation
-                StartCoroutine(triggerAnimation());
-                if(position == posicionCorrecta){
-                    tuberiasCorrectas++;
-                    if(tuberiasCorrectas == 5){
-                        Debug.Log("Puzzle resuelto");
+        {   
+            if(tuberiasCorrectas < 5 && gameObject.tag == "Tuberia"){
+                // Obtenemos el rayo desde la cámara al punto donde hicimos clic
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 5.0f);
+                // Si el rayo golpea al GameObject
+                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == null)
+                {   Debug.Log("rayo colisiona con"+hit.collider.gameObject.name);
+                    
+                }
+                if (Physics.Raycast(ray, out hit) && hit.collider.gameObject == gameObject)
+                {   Debug.Log("Tuberia clickeada");
+                    // Activamos el trigger de la animación
+                    position=(position+1) % 4;
+                    // lanza la funcion triggerAnimation
+                    StartCoroutine(triggerAnimationTub());
+                    if(position == posicionCorrecta){
+                        tuberiasCorrectas++;
+                        GetComponent<BoxCollider>().enabled = false;
+                        if(tuberiasCorrectas == 5){
+                            Debug.Log("Tuberias colocadas correctamente");
+                        }
                     }
                 }
             }
+            else{
+                if(tuberiasCorrectas == 5 && gameObject.tag == "Manivela"){
+                    StartCoroutine(triggerAnimationManivela());
+                    GetComponent<BoxCollider>().enabled = false;
+                    Debug.Log("Manivela girada, Puzzle completado");
+                }
+            }
+            
         }
+        
     }
 
+    IEnumerator triggerAnimationManivela(){
+        animator.SetTrigger("giraManivela");
+        yield return new WaitForSeconds(1.5f);
+        animator.ResetTrigger("giraManivela");
+    }
 
-    IEnumerator triggerAnimation(){
+    IEnumerator triggerAnimationTub(){
         animator.SetTrigger("rot"+position.ToString());
         yield return new WaitForSeconds(1.0f);
         animator.ResetTrigger("rot"+position.ToString());
